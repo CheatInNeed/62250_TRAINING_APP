5b80759b81029ebe591c7e7bebc5786e3b5dcc42
lille <lillerupjr@gmail.com>
2026-01-12 10:47:04 +0100

    changed DEBUG_WIPE_DB to false

--- app/src/main/java/com/example/gymlocker/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/gymlocker/data/database/AppDatabase.kt
@@ -52,7 +52,7 @@ abstract class AppDatabase : RoomDatabase() {
         private const val DB_NAME = "gymlocker.db"
 
         // 🔥 Toggle this when debugging
-        private const val DEBUG_WIPE_DB = true
+        private const val DEBUG_WIPE_DB = false
 
         fun getDatabase(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
